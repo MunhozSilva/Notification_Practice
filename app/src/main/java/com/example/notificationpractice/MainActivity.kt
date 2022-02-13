7a45c173120b99958b3131977a24b5eed805ac82
MunhozSilva <luciano.asimov@gmail.com>
2022-02-13 19:35:17 -0300

    Bug fixed

--- app/src/main/java/com/example/notificationpractice/MainActivity.kt
+++ app/src/main/java/com/example/notificationpractice/MainActivity.kt
@@ -25,19 +25,25 @@ class MainActivity : AppCompatActivity() {
                 .setContentText(getString(R.string.notification_text))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
+            with(NotificationManagerCompat.from(this)) {
+                notify(1, builder.build())
+            }
 
+            createNotificationChannel()
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES .0) {
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel("channel_id", name, importance).apply {
                 description = descriptionText
             }
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
-
-        NotificationManagerCompat.from(this).notify(41, builder.build())
-
-
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
     }
 }
